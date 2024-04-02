ac8ea4d19c5d0c1304dae69fc8a6254e79757c74
BearCubsLOL <student.187827@worcesterschools.net>
2024-04-02 17:20:52 -0400

    Update

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import random
 
-computer = random.randint(1,3)
+def run():
+    computer = random.randint(1,3)
 
-userChoice = int(input("Input a number 1 - 3 (1 is rock, 2 is paper, and 3 is scissors): "))
+    userChoice = int(input("Input a number 1 - 3 (1 is rock, 2 is paper, and 3 is scissors): "))
 
-if computer == userChoice:
-    print("Its A Tie!")
-elif computer > userChoice or computer == 1 and userChoice == 3:
-    print("Computer Wins!")
-elif userChoice > computer or userChoice == 1 and computer == 3:
-    print("You Win!")
+    if 4 > userChoice > 0:
+        if computer == userChoice:
+            print("Its A Tie!")
+        elif computer > userChoice or computer == 1 and userChoice == 3:
+            print("Computer Wins!")
+        elif userChoice > computer or userChoice == 1 and computer == 3:
+            print("You Win!")
+    else:
+        try_again = input("Invalid number would you like to try again? ")
+        if try_again.capitalize == "Yes":
+            run()
+        else:
+            pass
+run()
